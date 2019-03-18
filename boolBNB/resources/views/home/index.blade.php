b659a7c4dd6e4c8cffd20204274db781e10e45ff
DiegoAneli <diegoaneli@gmail.com>
2019-03-18 14:42:00 +0100

    prime rotte

---
+++ boolBNB/resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+Ciao boolb&b
+@endsection
--- boolBNB/routes/web.php
+++ boolBNB/routes/web.php
@@ -11,10 +11,42 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//admission page
 
-Route::get('/ciao', function () {
-    return view ('ciao');
-});
+Route::get('/iscriviti', 'AdmissionController@index')->name('admission.index');
+Route::post('/iscriviti', 'AdmissionController@save')->name('admission.save');
+
+
+
+Route::get('/', 'HomeController@index')->name('home');
+
+//static page
+
+Route::get('/privacyPolicy', 'StaticPageController@privacyPolicy')->name('static.privacy');
+Route::get('/workWithUs', 'StaticPageController@workWithUs')->name('static.work_with_us');
+
+//public page
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/search', 'searchController@index')->name('search');//ricerca apt su db
+Route::get('/apartment', 'apartmentController@index')->name('apartment');//ricerca dettaglio apt su db
+Route::get('/aptfilter', 'aptfilterController@index')->name('apartment');//ricerca filtri su db
+//form da includere in public
+
+
+
+//private page PROPRIETARIO
+
+Route::resource('/users', 'UserController');
+
+
+
+
+
+//navbar page
+
+Route::get('/viaggi', 'TripsController@index')->name('trips');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
