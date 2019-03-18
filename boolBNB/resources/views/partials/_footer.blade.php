b2ff72fb190783bb716140cd17da7e0151b6193b
LeonardoSarallo <leo.sarallo@gmail.com>
2019-03-18 14:00:09 +0100

    footer

---
+++ boolBNB/resources/views/partials/_footer.blade.php
@@ -0,0 +1,122 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+              <h3>Boolbnb</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    Opportunita di lavoro
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Stampa
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Condizioni
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Aiuto
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Diversita e appartenenza
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Informazioni di contatto
+                  </a>
+                </li>
+              </ul>
+
+            </div>
+            <div class="col-3">
+              <h3>Scopri</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    Affidabilita e sicurezza
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Crediti di viaggio
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Cittadino di Boolbnb
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Viaggio di lavoro
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Guide
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    BoolbnbMag
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Eventi
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-3">
+              <h3>Ospita</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    Perche affittare
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Ospitalita
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Ospitare responsabilmente
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Community Center
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Offri un esperienza
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Open Homes
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-3">
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </div>
+    </div>
+</footer>
