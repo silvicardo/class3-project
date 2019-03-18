a0b52f6c03cf27c5e6671a603294b39a2af24987
MassiBaldi <massibaldi85@gmail.com>
2019-03-18 15:22:59 +0100

    navbar

--- boolBNB/resources/views/partials/_header.blade.php
+++ boolBNB/resources/views/partials/_header.blade.php
@@ -1,1 +1,28 @@
-<h1>ciao</h1>
+<div class="nav_bar">
+  <div class="container">
+    <div class="nav_bar_left">
+      <img src="{{ asset('img/logo.png') }}" alt="">
+      <div class="search">
+        <div class="search_img">
+          <i class="fas fa-search"></i>
+        </div>
+        <input type="text" placeholder="Cerca appartamento" value="">
+      </div>
+    </div>
+    <div class="nav_bar_right">
+      <ul>
+        <li>
+          <a href="#">Diventa un host</a>
+        </li>
+        <li>
+          <a href="#">Registrati</a>
+        </li>
+        <li>
+          <a href="#">Accedi</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+
+</div>
