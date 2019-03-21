336f89727f19885be9a9c08074d74476ca397328
MassiBaldi <massibaldi85@gmail.com>
2019-03-21 12:53:42 +0100

    menu a tendina per appartamenti e profilo, pagina profilo, aggiornato form con value

--- boolBNB/resources/views/apartment/show.blade.php
+++ boolBNB/resources/views/apartment/show.blade.php
@@ -37,4 +37,38 @@
   @endif
 </div>
 
+<div class="container">
+  <div class="row">
+    <div class="col-sm-5">
+      <div class="card" style="width: 18rem;">
+        <div class="card_user">
+          <img class="img_user card-img-top" src="{{ asset('img/avatar1.png') }}" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">"User"</h5>
+          <h6 class="card-title">"Email"</h6>
+          <h6 class="card-title">"Registrato il"</h6>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-7">
+      <div class="container_profile">
+        <div class="container_profile_edit">
+          <h2>Ciao, Massmiliano</h2>
+          <a href="#">Modifica profilo</a>
+        </div>
+        <div class="container_profile_delete">
+          <form action="#" method="POST">
+                     @csrf
+                     @method('DELETE')
+                     <button type="submit" class="btn btn-danger">Elimina il tuo account</button>
+           </form>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
 @endsection
