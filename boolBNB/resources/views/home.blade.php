e7898b33ef9769b0ce6388333c54728b607e0267
LeonardoSarallo <leo.sarallo@gmail.com>
2019-03-21 17:08:37 +0100

    sponsor jumbo e varie

--- boolBNB/resources/views/home.blade.php
+++ boolBNB/resources/views/home.blade.php
@@ -2,31 +2,51 @@
 
 @section('content')
   <div class="jumbo">
+  <div class="container">
+    <div class="row">
+            @foreach($allApartments as $key => $apartment)
+              <div class="col-md-4 mb-5">
+                <div href="{{route('apartment.show', $apartment->id) }}">
+                  <div class="card">
+                   <img class="card-img-top" src="{{ $apartment->image_url}}" alt="Card image cap">
+                   <div class="card-body">
+                     <h5 class="card-title">{{ $apartment->title }}</h5>
+                     <p class="card-text">{{ $apartment->description}}</p>
 
-
-    <div class="container">
-
-      <div class="cardcontainer">
-        @foreach(array_chunk($allApartments->all(), 3) as $row)
-             <div class="row">
-                  @foreach($row as $allApartments)
-                    <a href="{{route('apartment.show', $allApartments->id) }}">
-                      <div class="card" style="width: 16rem;">
-                       <img class="card-img-top" src="{{ $allApartments->image_url}}" alt="Card image cap">
-                       <div class="card-body">
-                         <h5 class="card-title">{{ $allApartments->title }}</h5>
-                         <p class="card-text">{{ $allApartments->description}}</p>
-                       </div>
-                      </div>
-                    </a>
-
-                  @endforeach
-             </div>
-        @endforeach
-      </div>
-
+                   </div>
+                  </div>
+                </div>
+              </div>
+            @endforeach
     </div>
+  </div>
+
+
 
   </div>
 
 @endsection
+
+
+{{-- <div class="container">
+
+  <div class="cardcontainer">
+    @foreach(array_chunk($allApartments->all(), 3) as $row)
+         <div class="row">
+              @foreach($row as $allApartments)
+                <a href="{{route('apartment.show', $allApartments->id) }}">
+                  <div class="card" style="width: 16rem;">
+                   <img class="card-img-top" src="{{ $allApartments->image_url}}" alt="Card image cap">
+                   <div class="card-body">
+                     <h5 class="card-title">{{ $allApartments->title }}</h5>
+                     <p class="card-text">{{ $allApartments->description}}</p>
+                   </div>
+                  </div>
+                </a>
+
+              @endforeach
+         </div>
+    @endforeach
+  </div>
+
+</div> --}}
