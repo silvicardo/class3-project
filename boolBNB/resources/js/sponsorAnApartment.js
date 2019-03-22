bcac05cfa10a2619b9fa4273077ce62cbd9eae71
Riccardo Silvi <silvicardo@gmail.com>
2019-03-22 05:50:36 +0100

    sponsor plan Picker e boilerplate dropin braintree

--- boolBNB/resources/js/sponsorAnApartment.js
+++ boolBNB/resources/js/sponsorAnApartment.js
@@ -1,3 +1,4 @@
+require('./bootstrap');
 var braintree = require('braintree');
 var $ = require("jquery");
 
@@ -6,6 +7,21 @@ var $ = require("jquery");
 $(document).ready(function(){
 
   console.log('braintree script');
+
+  //BRAINTREE DROPIN
+
+  braintree.dropin.create({
+        authorization: 'CLIENT_TOKEN_FROM_SERVER',
+        container: '#dropin-container'
+      }, function (createErr, instance) {
+        
+
+        button.addEventListener('click', function () {
+          instance.requestPaymentMethod(function (err, payload) {
+            // Submit payload.nonce to your server
+          });
+        });
+      });
 
 
 })
--- boolBNB/resources/views/admin/owner/sponsor.blade.php
+++ boolBNB/resources/views/admin/owner/sponsor.blade.php
@@ -6,22 +6,34 @@
 
 
   <div class="container">
-    <div class="sponsor">
+    <div class="sponsor p-5">
       <div class="row">
         <div class="col-12">
-          <h2 class="sponsortitle">Benvenuto {{ $owner->name }}, sponsorizza il tuo appartamento {{ $apartment->title}} </h2>
+          <h2 class="sponsortitle">Benvenuto {{ $owner->name }},<br>sponsorizza il tuo appartamento<br> {{ $apartment->title}} </h2>
+          <form >
+            <div class="form-group">
+              <h3 class="sponsorlabel">Scegli la tua sponsorizzazione:</h3>
+              <select class="form-control form-control-lg">
+                @foreach ($plans as $plan)
+                <option value="{{ $plan->slug }}">{{ $plan->description }}</option>
+                @endforeach
+              </select>
+            </div>
+            <div id="dropin-container">
+              {{-- QUi finisce il dropin di Braintree  --}}
+            </div>
+            {{-- Da attivare quando il seeder sarà pronto, per ora appartamento da url --}}
+            {{-- <div class="form-group">
+              <h3 class="sponsorlabel">Scegli uno dei tuoi appartamenti:</h3>
+              <select class="form-control form-control-lg">
+                @foreach ($ownerApartments as $apartment)
+                <option value="{{ $apartment->id }}">{{ $apartment->title }}</option>
+                @endforeach
+              </select>
+            </div> --}}
 
-          <form class="form-group">
-            <h3 class="sponsorlabel">Scegli la tua sponsorizzazione:</h3>
-            @foreach ($plans as $plan)
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                <label class="form-check-label" for="exampleRadios1">
-                   {{ $plan->description }}
-                </label>
-              </div>
-            @endforeach
           </form>
+
           </div>
         </div>
       </div>
