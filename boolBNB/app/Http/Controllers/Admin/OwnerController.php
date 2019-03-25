0bb0fab2089b9850c056db05f3aa747681236a02
DiegoAneli <diegoaneli@gmail.com>
2019-03-25 11:15:34 +0100

    Sistemati costruttori in controller

--- boolBNB/app/Http/Controllers/Admin/OwnerController.php
+++ boolBNB/app/Http/Controllers/Admin/OwnerController.php
@@ -9,6 +9,41 @@ use App\User;
 class OwnerController extends Controller
 {
 
+      //variabile per conservare l'utente
+      // che passa sul controller
+      //e il suo ruolo
+      protected $currentUser;
+
+      //middleware permessi sul costruttore
+      public function __construct(){
+
+        $this->currentUser = null;
+
+        //1.se non sei loggato puoi accedere solo ad index e a show
+        $this->middleware('auth'); //NON PASSATO? REGISTER O LOGIN
+
+        //tutti gli user registrati hanno permesso di
+        //gestire la propria messaggistica,
+        //esplicitiamo comunque questa possibilità
+        $this->middleware('permission:manage-owner');
+
+        //In caso non si soddisfino le proprietà si riviene
+        //mandati alla pagina 403:forbidden
+
+        //Popoliamo la var user del controller
+        //per non dover ripetere la ricerca ogni volta
+        $this->middleware(function ($request, $next) {
+
+          $this->currentUser = Auth::user();
+
+          $this->currentUser->role = $this->currentUser->roles()->first()->name;
+
+
+          return $next($request);
+
+        });
+
+      }
 
     /**
      * Display the specified resource.
